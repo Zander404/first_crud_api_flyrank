f40798b652fb8445b1cf9dadeb8bf1e3adf21894
xandy <alexandrearthurpg@gmail.com>
2026-07-15 23:42:10 -0300

    Stage 6: publish and docs

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from tasks.routes import router as tasks_routes
 
-app = FastAPI(title="Task Api", version="1.0", summary="Api to create tasks")
+app = FastAPI(
+    title="Task Api", version="1.0", summary="Api to create tasks", redoc_url="/redocs"
+)
 
 
 @app.get("/")
